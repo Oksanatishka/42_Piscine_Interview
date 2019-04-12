6e10d36865b91cbe83e55c8939fe9d4673a6012b
Oksana Bibik <Oksanatishka@users.noreply.github.com>
2019-04-12 13:12:42 -0700

    Update occupiedPlaces.c

--- D04/ex09/occupiedPlaces.c
+++ D04/ex09/occupiedPlaces.c
@@ -6,6 +6,6 @@ int occupiedPlaces(unsigned int parkingRow)
 {
 	int i = 0;
 	for (i = 0; parkingRow; i++)
-		parkingRow = parkingRow & (parkingRow -1);
+		parkingRow = parkingRow & (parkingRow -1);	// parkingRow &= (parkingRow - 1);
 	return i;
 }
